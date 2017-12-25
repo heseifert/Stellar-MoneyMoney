44a5dfe5f4ba6a8854645f8c4cd1d1e2bbefa6e0
Heinrich Seifert <heinrich.seifert@me.com>
2017-12-25 22:28:35 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Stellar-MoneyMoney
+Fetches amount and value of privately held Stellar address via horizon.stellar.org and returns it as a security.  
+This can be also be used to track your cold storage.
+
+Do you like this extension? Donate to Stellar address: `Xu3GNVJwdf3uUxA7xZ5UVQxACR8PbA9CPF`
+
+Thanks to Jacubeit and this Repository https://github.com/Jacubeit/Bitcoin-MoneyMoney/ for the Inspiration.
+
+## Extension Setup
+
+You can get a signed version of this extension from
+
+* my [GitHub releases page](https://github.com/heseifert/Stellar-MoneyMoney/releases/tag/v0.1), or
+* the [MoneyMoney Extensions](https://moneymoney-app.com/extensions/) page
+
+Once downloaded, move `Stellar.lua` to your MoneyMoney Extensions folder.
+
+**Note:** This extension requires MoneyMoney **Version 2.2.19** or newer.
+
+## MoneyMoney Setup
+
+Add a new account (type "Stellar"). 
+
+**Use your Stellar adresses coma seperated as user name**  
+`1KuWLoZuoJgz3N6sLoAwGth9XGm8YuFTGt, 1KuWLoZuoJgz3N6sLoAwGth9XGm8YuFTGt` (example)
+
+**Use whatever you want as password**  
+`123` (example)
---
+++ Stellar.lua
@@ -0,0 +1,107 @@
+-- Inofficial Stellar (XLM) Extension for MoneyMoney
+-- Fetches Stellar (XLM) quantity for addresses via blockexplorer API
+-- Fetches Stellar (XLM) price in EUR via coinmarketcap API
+-- Returns cryptoassets as securities
+--
+-- Username: Stellar Adresses comma seperated
+-- Password: [Whatever]
+
+-- MIT License
+
+-- Copyright (c) 2017 heseifert
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+
+WebBanking{
+  version = 0.1,
+  description = "Include your Stellar as cryptoportfolio in MoneyMoney by providing Stellar (XLM) addresses as usernme (comma seperated) and a random Password",
+  services= { "Stellar" }
+}
+
+local stellarAddress
+local connection = Connection()
+local currency = "EUR" -- fixme: make dynamik if MM enables input field
+
+function SupportsBank (protocol, bankCode)
+  return protocol == ProtocolWebBanking and bankCode == "Stellar"
+end
+
+function InitializeSession (protocol, bankCode, username, username2, password, username3)
+  stellarAddress = username:gsub("%s+", "")
+end
+
+function ListAccounts (knownAccounts)
+  local account = {
+    name = "Stellar",
+    accountNumber = "Crypto Asset Stellar",
+    currency = currency,
+    portfolio = true,
+    type = "AccountTypePortfolio"
+  }
+
+  return {account}
+end
+
+function RefreshAccount (account, since)
+  local s = {}
+  prices = requestStellarPrice()
+
+  for address in string.gmatch(stellarAddress, '([^,]+)') do
+    stellarQuantity = requestStellarQuantityForStellarAddress(address)
+
+    s[#s+1] = {
+      name = address,
+      currency = nil,
+      market = "cryptocompare",
+      quantity = stellarQuantity,
+      price = prices["price_eur"],
+    }
+  end
+
+  return {securities = s}
+end
+
+function EndSession ()
+end
+
+
+-- Querry Functions
+function requestStellarPrice()
+  response = connection:request("GET", cryptocompareRequestUrl(), {})
+  json = JSON(response)
+
+  return json:dictionary()[1]
+end
+
+function requestStellarQuantityForStellarAddress(stellarAddress)
+  response = connection:request("GET", stellarRequestUrl(stellarAddress), {})
+  json = JSON(response)
+  return json:dictionary()["balances"][1]["balance"]
+end
+
+-- Helper Functions
+
+function cryptocompareRequestUrl()
+  return "https://api.coinmarketcap.com/v1/ticker/stellar/?convert=EUR"
+end
+
+function stellarRequestUrl(stellarAddress)
+  return "https://horizon.stellar.org/accounts/" .. stellarAddress
+end
